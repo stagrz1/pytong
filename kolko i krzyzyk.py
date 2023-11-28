d0cfd1e388f0c928dcf382eaa7ac7c8813c7a9e7
stagrz1 <stagrz@op.pl>
2023-11-28 09:50:36 +0100

    a

---
+++ kolko i krzyzyk.py
@@ -0,0 +1,14 @@
+board=  ["-","-","-",
+           "-","-","-",
+           "-","-","-",]
+gracz= "X"
+zwyciezca= None
+uruchomionaGra=True
+
+
+def printBoard(board):
+    print(board[0]+ " | " + board[1] + " | " +board[2])
+    print(board[4]+ " | " + board[5] + " | " +board[6])
+    print(board[7]+ " | " + board[8] + " | " +board[9])
+    
+     
