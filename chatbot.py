1d7a79af178161afef3a1712399fbc4a2ad7f1bc
stagrz1 <stagrz@op.pl>
2023-12-05 10:24:18 +0100

    a

---
+++ chatbot.py
@@ -0,0 +1,20 @@
+import random
+
+
+def chatbot():
+     responses = {
+        "Jak się masz?": ["Dobrze, dziękuję!", "Nieźle, a u Ciebie?", "Mam się świetnie!"],
+        "Co u Ciebie słychać?": ["Wszystko w porządku.", "Trochę zmęczony, ale ogólnie dobrze.", "Mam się świetnie!"],
+        "Jaka jest pogoda?": ["Pogoda jest piękna!", "Deszczowo niestety.", "Chmury, ale bez deszczu."],
+        "Czy masz jakieś plany na dzisiaj?": ["Nie, jeszcze nie planuję nic konkretnego.", "Będę pracować nad kilkoma projektami.", "Może pójdę na spacer."],
+        "Jak się nazywasz?" : ["Nie mam konkretnego imienia, ale mozesz na mnie mówić Chatty."],
+        "Ile masz lat?" : ["W tym roku obchodzę swoje drugie urodziny."],
+        "Jaki jest twój ulubiony kolor?" : ["Moim ulubionym kolorem jest zielony."],
+
+
+
+
+
+
+        "Do widzenia": ["Do zobaczenia!", "Żegnaj!", "Miłego dnia!"],
+    }
