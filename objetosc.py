f842c1669e6c574057039ae61aed25641571a024
stagrz1 <stagrz@op.pl>
2023-12-19 09:30:12 +0100

    a

--- objetosc.py
+++ objetosc.py
@@ -16,20 +16,39 @@ def objetosc_walca(r,h ):
 def objetosc_prostopadloscianu(a,b,c):
     return a * b * c
 
-print("Objetosc jakiej bryły chcesz dziś obliczyc?")
-print("1. Sześcian")
-print("2. Kula")
-print("3. prostopadłoscian")  
-print("4. Walec")    
+while True:
+    print("Objetosc jakiej bryły chcesz dziś obliczyc?")
+    print("1. Sześcian")
+    print("2. Kula")
+    print("3. prostopadłoscian")  
+    print("4. Walec")    
+    
 
+    wybor_uzytkownika=input("wybierz numer od 1-5")
 
-wybor_uzytkownika=int(input("wybierz numer od 1-4"))    
+    if wybor_uzytkownika == '1':
+            a = float(input("Podaj długość boku szescianu: "))
+            objetosc  = objetosc_szescianu(a)
 
-if wybor_uzytkownika == '1':
-            promien = float(input("Podaj długość boku szescianu: "))
-            objetosc  = objetosc_szescianu
-
-elif wybor_uzytkownika == '2':
+    elif wybor_uzytkownika == '2':
             promien = float(input("Podaj promień kuli: "))
-            objetosc = objetosc_kuli
-elif wybpr_uzyw
+            objetosc = objetosc_kuli(promien)
+
+    elif wybor_uzytkownika == '3':
+       a = float(input("Podaj długość boku podstawy: "))
+       b = float(input("Podaj długość 2 boku podstawy: "))
+       c = float(input("Podaj wysokość prostopadloscianu : "))
+       objetosc = objetosc_prostopadloscianu(a,b,c)
+
+    elif wybor_uzytkownika == '4':
+       r = float(input("podaj dlugosc promienia podstawy: "))
+       h = float(input("Podaj wysokosc walca: "))
+       objetosc = objetosc_walca(r,h)
+    
+    
+    else:   
+            print("Nieprawidłowy wybór. Spróbuj ponownie.")
+            continue
+
+    print(f"Objętość bryły: {objetosc:.2f}")
+
